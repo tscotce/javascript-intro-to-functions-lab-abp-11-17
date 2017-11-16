87ffbe345bfc64fa6283f3dfc67725e2ee40f004
tscotce <tscotce@gmail.com>
2017-11-16 18:22:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function logShout (string) {
 function logWhisper (string) {
   console.log (string.toLowerCase());
 }
+function sayHiToGrandma (string) {
+  return string.toLowerCase () === string;
+  return string.toUpperCase () === string;
+}
