05656589fefa272e27f8dd09c378fe13c57e7077
tscotce <tscotce@gmail.com>
2017-11-16 18:07:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function shout (string) {
-  console.log(string.toUpperCase);
   return string.toUpperCase()
 }
 function whisper (string) {
   return string.toLowerCase()
+}
+function logShout(string) {
+  console.log string.toUpperCase()
 }
