9e70ccc319ff715b9c3a8d22c02eb4e4fc80bfaf
tscotce <tscotce@gmail.com>
2017-11-16 18:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout (string) {
 function logWhisper (string) {
   console.log (string.toLowerCase());
 }
-function sayHiToGrandma (string) {
-  if (string.toLowerCase === string) {
-  "I can't hear you";
-}
+//function sayHiToGrandma (string) {
+//  if (string.toLowerCase === string) {
+//  "I can't hear you";
+//}
